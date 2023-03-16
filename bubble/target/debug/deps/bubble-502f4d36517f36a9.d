5fab397d21994e22993ad109380c06cfee8c4a0e
BlackBLuo <72017161+BlackBLuo@users.noreply.github.com>
2023-03-17 07:13:45 +0800

    Add files via upload

---
+++ bubble/target/debug/deps/bubble-502f4d36517f36a9.d
@@ -0,0 +1,5 @@
+c:\Users\86188\Desktop\substrate\rust\bubble\target\debug\deps\bubble-502f4d36517f36a9.rmeta: src\main.rs
+
+c:\Users\86188\Desktop\substrate\rust\bubble\target\debug\deps\bubble-502f4d36517f36a9.d: src\main.rs
+
+src\main.rs:
---
+++ bubble/target/debug/deps/bubble-a8d922472de0e8ce.d
@@ -0,0 +1,5 @@
+c:\Users\86188\Desktop\substrate\rust\bubble\target\debug\deps\bubble-a8d922472de0e8ce.rmeta: src\main.rs
+
+c:\Users\86188\Desktop\substrate\rust\bubble\target\debug\deps\bubble-a8d922472de0e8ce.d: src\main.rs
+
+src\main.rs:
