518078469186eddb87e51bc20845d9cbde329e78
BlackBLuo <72017161+BlackBLuo@users.noreply.github.com>
2023-03-25 00:13:59 +0800

    Update main.rs

--- add/src/main.rs
+++ add/src/main.rs
@@ -7,7 +7,13 @@ trait FromStrRadixHelper:
     fn checked_sub(&self, other: u32) -> Option<Self>;
     fn checked_add(&self, other: u32) -> Option<Self>;
 }
-
+pub fn sum(arr: &[u32]) -> Result<u32, String> {
+    let s: u64 = arr.iter().map(|e| e.to_owned()).map(|e| e as u64).sum();
+    if s > u32::MAX as u64 {
+        return Err("over max u32".to_string());
+    }
+    return Ok(s as u32);
+}
 fn main() {
     let a: u32 ;
     let b: u32 = a.wrapping_add(1);      // 所有模式下都按照补码循环溢出规则
